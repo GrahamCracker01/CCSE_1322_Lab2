27bb8c4bc71ce41b74a27609ac183504d99a33e3
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2023-01-19 19:06:33 -0500

    Initial Commit

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Lab2 {
+//Class: CSE 1322L
+//Section: J03
+//Term: Spring 2023
+//Name: Asher Graham
+
+import java.util.Scanner;
+class Lab2 {
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in);
+        int intQuit = 0, intInput;
+        float fltInput;
+
+        //create objects
+        StockItem milk = new StockItem("1 Gallon of Milk", 3.60f, 15);
+        StockItem bread = new StockItem("1 Loaf of Bread", 1.98f, 30);
+
+        //remember System.out.printf("%.2f", milk.getPrice()); to print correctly formatted price
+        //main loop
+        do {
+            System.out.println("1. Sold One Milk\n" +
+                    "2. Sold One Bread\n" +
+                    "3. Change price of Milk\n" +
+                    "4. Change price of Bread\n" +
+                    "5. Add Milk to Inventory\n" +
+                    "6. Add Bread to Inventory\n" +
+                    "7. See Inventory\n" +
+                    "8. Quit");
+            switch (Integer.parseInt(scan.nextLine())) {
+                case 1:
+                    milk.lowerQuantity();
+                    break;
+                case 2:
+                    bread.lowerQuantity();
+                    break;
+                case 3:
+                    System.out.println("What is the new Price for milk?");
+                    fltInput = Float.parseFloat(scan.nextLine());
+                    milk.setPrice(fltInput);
+                    break;
+                case 4:
+                    System.out.println("What is the new Price for bread?");
+                    fltInput = Float.parseFloat(scan.nextLine());
+                    bread.setPrice(fltInput);
+                    break;
+                case 5:
+                    System.out.println("How many milk did we get?");
+                    intInput = Integer.parseInt(scan.nextLine());
+                    milk.raiseQuantity(intInput);
+                    break;
+                case 6:
+                    System.out.println("How many bread did we get?");
+                    intInput = Integer.parseInt(scan.nextLine());
+                    bread.raiseQuantity(intInput);
+                    break;
+                case 7:
+                    System.out.println("Milk: "+milk+"\nBread: "+bread);
+                    break;
+                case 8:
+                    intQuit++;
+                    break;
+                default:
+                    System.out.println("Please input a valid number.");
+                    break;
+            }
+        } while(intQuit==0);
+    }
 }
--- src/StockItem.java
+++ src/StockItem.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class StockItem {
+//Class: CSE 1322L
+//Section: J03
+//Term: Spring 2023
+//Name: Asher Graham
+
+import java.text.DecimalFormat;
+
+public class StockItem {
+
+    //declare variables
+    private String description;
+    private int id;
+    private float price;
+    private int quantity;
+    private static int count = 0;
+    private static final DecimalFormat decFormat = new DecimalFormat("0.00");
+
+    //default constructor
+    public StockItem() {
+        description = "null";
+        id = (count);
+        count++;
+        price = 0;
+        quantity = 0;
+    }
+
+    //overloaded constructor
+    public StockItem(String newDescription, float newPrice, int newQuantity) {
+        description = newDescription;
+        id = count;
+        count++;
+        price = newPrice;
+        quantity = newQuantity;
+    }
+
+    //toString
+    public String toString() {
+        return ("Item number: "+id+" is "+description+" has price "+ decFormat.format(price)+" we currently have "+quantity+" in stock");
+    }
+
+    //getter methods
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    //setter methods
+    public void setPrice(float newPrice) {
+        if (newPrice>0) {
+            price = newPrice;
+        }
+        else {
+            System.out.println("Error: price must be more than $0.00");
+        }
+    }
+
+    public void lowerQuantity() {
+        if (quantity > 0) {
+            quantity -= 1;
+        }
+        else {
+            System.out.println("There is no more product to sell!");
+        }
+    }
+
+    public void raiseQuantity(int newQuantity) {
+        quantity += newQuantity;
+    }
 }
